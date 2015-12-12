7353cf709b22aa0bab15fa0752aaf9a033035d0e
Hangfei Lin <hangfeilin@gmail.com>
2015-12-11 21:12:33 -0800

    Revert "Setup index page."
    This reverts commit 4cddde4daacdb4a53e36d010af240370d74ec2d5.

--- web/web/urls.py
+++ web/web/urls.py
@@ -17,9 +17,6 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from . import views
-
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
     url(r'^admin/', admin.site.urls),
 ]
