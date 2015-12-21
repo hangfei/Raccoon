492bec0224d659427157942e14cfe1d69f62d0cd
Hangfei Lin <hangfeilin@gmail.com>
2015-12-20 16:14:40 -0800

    Updated db connection to Amazon RDS and added requirements.txt.

--- enverest/enverest/settings.py
+++ enverest/enverest/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'enverest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'raccoon',
+        'USER': 'admin',
+        'PASSWORD': 'raccoonadmin',
+        'HOST': 'raccoon.czeip1lepbze.ap-southeast-1.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
