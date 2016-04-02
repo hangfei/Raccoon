ea5a13f7425b80986b2eebc91e8725f09f8727d3
bofwang <wangbofei123@gmail.com>
2016-04-02 23:20:19 +0100

    Change admin industries and expertise

--- src/common/admin.py
+++ src/common/admin.py
@@ -3,7 +3,10 @@ from django.contrib import admin
 from .models import Client, Expert, Project, CommentForExpert, CommentForClient, Image
 # Register your models here.
 admin.site.register(Client)
-admin.site.register(Expert)
+
+class ExpertAdmin(admin.ModelAdmin):
+    exclude = ('industries','expertise')
+admin.site.register(Expert, ExpertAdmin)
 admin.site.register(Project)
 admin.site.register(CommentForExpert)
 admin.site.register(CommentForClient)
